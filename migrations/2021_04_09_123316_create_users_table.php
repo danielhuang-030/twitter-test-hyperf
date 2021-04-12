249f56dec43aebe47b2960f5e61599f40e30698d
Daniel <daniel.huang@coolbe.com.tw>
2021-04-12 18:03:18 +0800

    (WIP)feat: user for auth

---
+++ migrations/2021_04_09_123316_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+use Hyperf\Database\Migrations\Migration;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Schema\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id')->comment('自動編號');
+            $table->string('name')->index()->nullable()->comment('名稱');
+            $table->string('email')->unique()->comment('email');
+            $table->string('username')->unique()->comment('帳號');
+            $table->string('password')->comment('密碼');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+}
